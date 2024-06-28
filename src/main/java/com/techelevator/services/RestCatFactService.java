9e963fecffad3b404128377a56d753183b1399e3
Eric Jacob <Explorer121@gmail.com[D[D[D[D[D[D[PJacob9[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[Cexplorer121@gmail.com>
2024-06-28 15:24:29 -0400

    Switching Partners

--- src/main/java/com/techelevator/services/RestCatFactService.java
+++ src/main/java/com/techelevator/services/RestCatFactService.java
@@ -3,14 +3,25 @@ package com.techelevator.services;
 import org.springframework.stereotype.Component;
 
 import com.techelevator.model.CatFact;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RestCatFactService implements CatFactService {
 
+	private static final String API_BASE_URL = "https://teapi.netlify.app/api/cats/";
+	private final RestTemplate restTemplate = new RestTemplate();
+
 	@Override
 	public CatFact getFact() {
-		// TODO Auto-generated method stub
-		return null;
+		CatFact catFact = null;
+		try {
+			catFact = restTemplate.getForObject(API_BASE_URL + "facts/random", CatFact.class);
+		} catch (RestClientResponseException | ResourceAccessException e) {
+			BasicLogger.log(e.getMessage());
+		}
+		return catFact;
 	}
 
 }
